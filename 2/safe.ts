deae1a63c54c3f43aa6908bfd13d14489f425ee9
James Acres <jamesacres@gmail.com>
2024-12-02 22:31:59 +0000

    Day two part two, bit scrappy

--- 2/safe.ts
+++ 2/safe.ts
@@ -1,4 +1,6 @@
-const checkIsSafe = (numbers: number[]) => {
+const checkIsSafe = (
+  numbers: number[],
+): { safe: boolean; unsafeIndex?: number } => {
   let forwards = false;
   for (const [indexString, number] of Object.entries(numbers)) {
     const index = Number(indexString);
@@ -16,24 +18,57 @@ const checkIsSafe = (numbers: number[]) => {
         number < previousNumber
       ) {
         // was forwards, now backwards, unsafe
-        return false;
+        return { safe: false, unsafeIndex: index };
       }
       if (
         !forwards && previousNumber !== undefined &&
         number > previousNumber
       ) {
         // was backwards, now forwards, unsafe
-        return false;
+        return { safe: false, unsafeIndex: index };
       }
     }
     if (
       previousNumber && ![1, 2, 3].includes(Math.abs(previousNumber - number))
     ) {
-      // greater than 1 or 2 distance
-      return false;
+      // greater than allowed distance
+      return { safe: false, unsafeIndex: index };
     }
   }
-  return true;
+  return { safe: true };
 };
 
-export { checkIsSafe };
+const checkIsSafeDampener = (numbers: number[]) => {
+  const { safe, unsafeIndex } = checkIsSafe(numbers);
+  if (safe) {
+    return true;
+  }
+
+  if (unsafeIndex) {
+    const newNumbers = [...numbers];
+    newNumbers.splice(unsafeIndex!, 1);
+    if (checkIsSafe(newNumbers).safe) {
+      return true;
+    }
+
+    if (unsafeIndex > 0) {
+      // Replace index before instead
+      const newNumbers = [...numbers];
+      newNumbers.splice(unsafeIndex - 1, 1);
+      if (checkIsSafe(newNumbers).safe) {
+        return true;
+      }
+    }
+
+    // Try to replace first index
+    const newNumbers2 = [...numbers];
+    newNumbers2.splice(0, 1);
+    if (checkIsSafe(newNumbers2).safe) {
+      return true;
+    }
+  }
+
+  return false;
+};
+
+export { checkIsSafe, checkIsSafeDampener };
--- 2/safe_test.ts
+++ 2/safe_test.ts
@@ -1,15 +1,64 @@
 import { assertEquals } from "@std/assert";
-import { checkIsSafe } from "./safe.ts";
+import { checkIsSafe, checkIsSafeDampener } from "./safe.ts";
 
 Deno.test(async function safeTotalWithInput() {
   const input = await Deno.readTextFile("./2/input.txt");
   const safeCount = input.split("\n").map((line) => line.split(/\s+/))
     .reduce(
       (result, numbers) => {
-        const isSafe = checkIsSafe(numbers.map(Number));
+        const isSafe = checkIsSafe(numbers.map(Number)).safe;
         return isSafe ? result + 1 : result;
       },
       0,
     );
   assertEquals(safeCount, 598);
+});
+
+Deno.test(function dampenerSafeWithExample1() {
+  assertEquals(checkIsSafeDampener([7, 6, 4, 2, 1]), true);
+});
+
+Deno.test(function dampenerSafeWithExample2() {
+  assertEquals(checkIsSafeDampener([1, 2, 7, 8, 9]), false);
+});
+
+Deno.test(function dampenerSafeWithExample3() {
+  assertEquals(checkIsSafeDampener([9, 7, 6, 2, 1]), false);
+});
+
+Deno.test(function dampenerSafeWithExample4() {
+  assertEquals(checkIsSafeDampener([1, 3, 2, 4, 5]), true);
+});
+
+Deno.test(function dampenerSafeWithExample5() {
+  assertEquals(checkIsSafeDampener([8, 6, 4, 4, 1]), true);
+});
+
+Deno.test(function dampenerSafeWithExample6() {
+  assertEquals(checkIsSafeDampener([1, 3, 6, 7, 9]), true);
+});
+
+Deno.test(function dampenerSafeWithChangeInDirection() {
+  assertEquals(checkIsSafeDampener([5, 4, 6, 7, 8]), true);
+});
+
+Deno.test(function dampenerSafeWithChangeInDirectionTwice() {
+  assertEquals(checkIsSafeDampener([5, 4, 6, 7, 6, 8]), false);
+});
+
+Deno.test(function dampenerSafeWithBadStart() {
+  assertEquals(checkIsSafeDampener([7, 4, 5, 6]), true);
+});
+
+Deno.test(async function dampenerSafeTotalWithInput() {
+  const input = await Deno.readTextFile("./2/input.txt");
+  const safeCount = input.split("\n").map((line) => line.split(/\s+/))
+    .reduce(
+      (result, numbers) => {
+        const isSafe = checkIsSafeDampener(numbers.map(Number));
+        return isSafe ? result + 1 : result;
+      },
+      0,
+    );
+  assertEquals(safeCount, 634);
 });
