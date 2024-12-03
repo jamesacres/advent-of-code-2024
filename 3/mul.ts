fe01a4cedbfbd3ea14bb8e3d976d516438333172
James Acres <jamesacres@gmail.com>
2024-12-03 22:06:38 +0000

    day three part two

--- 3/mul.ts
+++ 3/mul.ts
@@ -2,9 +2,23 @@ const extractMul = (input: string) => {
   return [...input.matchAll(new RegExp("mul\\([0-9]{1,3},[0-9]{1,3}\\)", "g"))];
 };
 
+const filterDonts = (input: string) => {
+  const donts = input.split(`don't()`);
+
+  // Always do first part up to first don't
+  const partsToDo = [donts.shift()];
+  // Each don't section we want to do everything after first do
+  donts.map((dont) => {
+    const [_ignoreUpToDo, ...doRest] = dont.split("do()");
+    partsToDo.push(...doRest);
+  });
+
+  return partsToDo.join("");
+};
+
 const runMul = (input: string) => {
   const [a, b] = input.replace("mul(", "").replace(")", "").split(",");
   return Number(a) * Number(b);
 };
 
-export { extractMul, runMul };
+export { extractMul, filterDonts, runMul };
--- 3/mul_test.ts
+++ 3/mul_test.ts
@@ -1,7 +1,7 @@
 import { assertEquals } from "@std/assert";
-import { extractMul, runMul } from "./mul.ts";
+import { extractMul, filterDonts, runMul } from "./mul.ts";
 
-Deno.test(async function safeTotalWithInput() {
+Deno.test(async function testSumMuls() {
   const input = await Deno.readTextFile("./3/input.txt");
   const lines = input.split("\n");
   const inputString = lines.join("");
@@ -11,4 +11,30 @@ Deno.test(async function safeTotalWithInput() {
     0,
   );
   assertEquals(total, 166630675);
+});
+
+Deno.test(async function testFilterDonts() {
+  const input = await Deno.readTextFile("./3/input.txt");
+  const lines = input.split("\n");
+  const inputString = lines.join("");
+  const filteredInputString = filterDonts(inputString);
+  assertEquals(filteredInputString.includes(`don't()`), false);
+  assertEquals(filteredInputString.includes(`mul(195,249)`), false);
+  assertEquals(filteredInputString.includes(`mul(316,793)`), true);
+});
+
+Deno.test(async function testFilterDontsSumMuls() {
+  const input = await Deno.readTextFile("./3/input.txt");
+  const lines = input.split("\n");
+  const inputString = lines.join("");
+  const filteredInputString = filterDonts(inputString);
+  assertEquals(filteredInputString.includes(`don't()`), false);
+  assertEquals(filteredInputString.includes(`mul(195,249)`), false);
+  assertEquals(filteredInputString.includes(`mul(316,793)`), true);
+  const muls = extractMul(filteredInputString);
+  const total = muls.map((mul) => runMul(String(mul))).reduce(
+    (result, number) => result + number,
+    0,
+  );
+  assertEquals(total, 93465710);
 });
