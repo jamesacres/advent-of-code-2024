b69bc5e05085880ebd84f0a504aff14f878cb5e2
James Acres <jamesacres@gmail.com>
2024-12-09 23:51:41 +0000

    day 9 part 2

--- 9/diskSpace.test.ts
+++ 9/diskSpace.test.ts
@@ -1,5 +1,10 @@
 import { assertEquals } from "@std/assert/equals";
-import { calculateChecksum, moveBlocks, toBlocks } from "./diskSpace.ts";
+import {
+  calculateChecksum,
+  moveBlocks,
+  moveBlocksV2,
+  toBlocks,
+} from "./diskSpace.ts";
 
 const example = "2333133121414131402";
 
@@ -29,5 +34,27 @@ Deno.test(async function inputDiskMapMoveBlocksChecksum() {
   assertEquals(
     calculateChecksum(moveBlocks(toBlocks(input, true))),
     6401092019345,
+  );
+});
+
+Deno.test(function exampleDiskMapMoveBlocksPart2() {
+  assertEquals(
+    moveBlocksV2(toBlocks(example, true)),
+    "0,0,9,9,2,1,1,1,7,7,7,.,4,4,.,3,3,3,.,..,.,5,5,5,5,.,6,6,6,6,.,...,.,8,8,8,8,..",
+  );
+});
+
+Deno.test(function exampleDiskMapMoveBlocksPart2ChecksumC() {
+  assertEquals(
+    calculateChecksum(moveBlocksV2(toBlocks(example, true))),
+    2858,
+  );
+});
+
+Deno.test(async function inputDiskMapMoveBlocksPart2Checksum() {
+  const input = await Deno.readTextFile("./9/input.txt");
+  assertEquals(
+    calculateChecksum(moveBlocksV2(toBlocks(input, true))),
+    6431472344710,
   );
 });
--- 9/diskSpace.ts
+++ 9/diskSpace.ts
@@ -50,9 +50,90 @@ const moveBlocks = (diskBlocks: string): string => {
 };
 
 const calculateChecksum = (diskBlocks: string): number => {
-  return diskBlocks.split(",").reduce((result, block, i) => {
-    return block === "." ? result : result + (Number(block) * i);
-  }, 0);
+  const adjustedInput = diskBlocks.replaceAll("..", ".,.").replaceAll(
+    "..",
+    ".,.",
+  );
+  return adjustedInput
+    .split(",").reduce((result, block, i) => {
+      return block === "." ? result : result + (Number(block) * i);
+    }, 0);
 };
 
-export { calculateChecksum, moveBlocks, toBlocks };
+const moveBlocksV2 = (diskBlocks: string): string => {
+  const spaceIndexes: number[] = [];
+  const files: {
+    [id: number]: {
+      indexes: number[];
+    };
+  } = {};
+  let i = 0;
+  const blocks = diskBlocks.split(/\[|\]/).filter((value) => value).reduce(
+    (result: { [key: number]: string }, block) => {
+      if (block.includes(".")) {
+        spaceIndexes.push(Number(i));
+        result[i] = block;
+        i = i + block.length;
+      } else {
+        if (!files[Number(block)]) {
+          files[Number(block)] = { indexes: [] };
+        }
+        files[Number(block)].indexes.push(Number(i));
+        result[i] = block;
+        i = i + 1;
+      }
+      return result;
+    },
+    {},
+  );
+
+  Object.keys(files).map(Number).sort((a, b) => b - a).forEach((fileId) => {
+    const fileLength = files[fileId].indexes.length;
+    const spaceIndexesIndex = spaceIndexes.findIndex((spaceIndex) =>
+      spaceIndex < files[fileId].indexes[0] &&
+      blocks[spaceIndex].length >= fileLength
+    );
+    if (spaceIndexesIndex !== -1) {
+      const spaceIndex = spaceIndexes[spaceIndexesIndex];
+      const remainingSpaces = blocks[spaceIndex].length - fileLength;
+
+      if (remainingSpaces) {
+        const remainingSpaceString = ".".repeat(remainingSpaces);
+        const remainingSpaceIndex = spaceIndex + fileLength;
+        const neighbourIndex = remainingSpaceIndex + remainingSpaces;
+        if (spaceIndexes.includes(neighbourIndex)) {
+          // Combine with existing space index
+          blocks[neighbourIndex] = `${
+            blocks[neighbourIndex]
+          }${remainingSpaceString}`;
+          spaceIndexes.splice(spaceIndexesIndex, 1);
+        } else {
+          // Replace with new space index
+          blocks[remainingSpaceIndex] = remainingSpaceString;
+          spaceIndexes.splice(spaceIndexesIndex, 1, remainingSpaceIndex);
+        }
+      } else {
+        spaceIndexes.splice(spaceIndexesIndex, 1);
+      }
+
+      blocks[files[fileId].indexes[0]] = ".".repeat(fileLength);
+      [...new Array(fileLength - 1)].forEach((_, i) => {
+        delete blocks[files[fileId].indexes[i + 1]];
+      });
+
+      // files[fileId].indexes.forEach((fileIndex) => {
+      //   blocks[fileIndex] = ".";
+      // });
+
+      files[fileId].indexes = [];
+      [...new Array(fileLength)].forEach((_, i) => {
+        const index = spaceIndex + i;
+        blocks[index] = `${fileId}`;
+        files[fileId].indexes.push(index);
+      });
+    }
+  });
+  return Object.values(blocks).join(",");
+};
+
+export { calculateChecksum, moveBlocks, moveBlocksV2, toBlocks };
