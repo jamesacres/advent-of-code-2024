de59291816b2f036cd0e2ddf42e24620691537f7
James Acres <jamesacres@gmail.com>
2024-12-11 22:41:46 +0000

    day 11 part 2 new approach

--- 11/blink.ts
+++ 11/blink.ts
@@ -1,17 +1,55 @@
-const blink = (input: string): string => {
-  return input.split(" ").reduce((result, part) => {
-    if (part === "0") {
-      return `${result.length ? `${result} ` : ""}1`;
-    }
-    if (part.length % 2 === 0) {
-      const [a, b] = [
-        Number(part.slice(0, part.length / 2)),
-        Number(part.slice(part.length / 2, part.length)),
-      ];
-      return `${result.length ? `${result} ` : ""}${a} ${b}`;
-    }
-    return `${result.length ? `${result} ` : ""}${Number(part) * 2024}`;
-  }, "");
+export type StoneMap = { [key in string]: number };
+const cache: StoneMap = {};
+
+const addStone = (result: StoneMap, stone: string) => {
+  if (result[stone] === undefined) {
+    result[stone] = 0;
+  }
+  result[stone] = result[stone] + 1;
 };
 
-export { blink };
+const stringToStoneCount = (input: string) =>
+  input.split(" ").reduce(
+    (result: StoneMap, stone) => {
+      addStone(result, stone);
+      return result;
+    },
+    {},
+  );
+
+const blinkStone = (stone: string, iteration: number): number => {
+  if (iteration === 0) {
+    return 1;
+  }
+  if (cache[`${stone}-${iteration}`]) {
+    return cache[`${stone}-${iteration}`];
+  }
+  let result;
+  if (stone === "0") {
+    result = blinkStone("1", iteration - 1);
+  } else if (stone.length % 2 === 0) {
+    const a = Number(stone.slice(0, stone.length / 2));
+    const b = Number(stone.slice(stone.length / 2, stone.length));
+    result = blinkStone(`${a}`, iteration - 1) +
+      blinkStone(`${b}`, iteration - 1);
+  } else {
+    const nextStone = Number(stone) * 2024;
+    result = blinkStone(`${nextStone}`, iteration - 1);
+  }
+  cache[`${stone}-${iteration}`] = result;
+  return result;
+};
+
+const blink = (stones: StoneMap, iteration: number): number => {
+  return Object.entries(stones).reduce((result, [stone, count]) => {
+    const thisResult = blinkStone(stone, iteration);
+    [...new Array(count)].forEach(
+      (_) => {
+        result = result + thisResult;
+      },
+    );
+    return result;
+  }, 0);
+};
+
+export { blink, stringToStoneCount };
